80f9b19b5badf99c701f8367affe8fb8da676ea8
Aleks <reppert@uni-potsdam.de>
2022-01-20 21:29:55 +0100

    update data_path naming

--- udkm/moke/functions.py
+++ udkm/moke/functions.py
@@ -7,32 +7,32 @@ teststring = "Successfully loaded udkm.moke.functions"
 
 # initialize some useful functions
 t0         =  0    # Estimated value of t0
-dataPath   = "data/"
-exportPath = "results/"
+data_path   = "data/"
+export_path = "results/"
 
 
 def load_data(date,time,voltage,col_to_plot):
-    file  = dataPath+str(date)+"_"+h.timestring(time)+"/Fluence/-1.0/"+str(voltage)+"/overviewData.txt"
+    file  = data_path+str(date)+"_"+h.timestring(time)+"/Fluence/-1.0/"+str(voltage)+"/overviewData.txt"
     data  = np.genfromtxt(file,comments = "#") 
     return(data[:,0], data[:,col_to_plot])
 
 def load_data_A(date,time,angle,col_to_plot):
-    file  = dataPath+str(date)+"_"+h.timestring(time)+"/Fluence/"+str(int(angle))+"/1.0"+"/overviewData.txt"
+    file  = data_path+str(date)+"_"+h.timestring(time)+"/Fluence/"+str(int(angle))+"/1.0"+"/overviewData.txt"
     data  = np.genfromtxt(file,comments = "#") 
     return(data[:,0], data[:,col_to_plot])
 
 def load_data_B(date,time,angle,col_to_plot):
-    file  = dataPath+str(date)+"_"+h.timestring(time)+"/Fluence/"+str(angle)+"/1.0"+"/overviewData.txt"
+    file  = data_path+str(date)+"_"+h.timestring(time)+"/Fluence/"+str(angle)+"/1.0"+"/overviewData.txt"
     data  = np.genfromtxt(file,comments = "#") 
     return(data[:,0], data[:,col_to_plot])
 
 def load_data_reflectivity(date,time,voltage,col_to_plot):
-    file  = dataPath+str(date)+"_"+h.timestring(time)+"/Fluence/-1.0/"+str(voltage)+"/dataBplus.txt"
+    file  = data_path+str(date)+"_"+h.timestring(time)+"/Fluence/-1.0/"+str(voltage)+"/dataBplus.txt"
     data  = np.genfromtxt(file,comments = "#") 
     return(data[:,0], data[:,col_to_plot])
 
 def load_data_hysteresis(date,time,name):
-    file  = dataPath+str(date)+"_"+h.timestring(time)+"/Fluence/Static/"+name+"_NoLaser.txt"
+    file  = data_path+str(date)+"_"+h.timestring(time)+"/Fluence/Static/"+name+"_NoLaser.txt"
     data  = np.genfromtxt(file,comments = "#") 
     return(data[:,0], data[:,1], data[:,2])
 
