da762b86570b5918775f0098e158f43fee01cbde
unknown <reppert@uni-potsdam.de>
2022-01-20 20:55:39 +0100

    updated constants

--- udkm/tools/constants.py
+++ udkm/tools/constants.py
@@ -1,5 +1,8 @@
 teststring = "Successfully loaded udkm.tools.constants"
 
-planckH        = 6.62607015e-34
-eV             = 1.602176634e-19
-c0             = 299792458
+H_PLANCK	= 6.62607015e-34
+EV			= 1.602176634e-19
+C_0			= 299792458
+K_B			= 1.380649e-23
+MU_0		= 1.25663706212e-6
+EPS_0		= 8.8541878128e-12
