864c4d025df592ed359d1f8d4aa271bf78e3734c
Aleks <reppert@uni-potsdam.de>
2022-02-21 14:46:13 +0100

    minor corrections in moke

--- udkm/moke/functions.py
+++ udkm/moke/functions.py
@@ -17,19 +17,19 @@ def load_data(date, time, voltage, col_to_plot):
     return(data[:, 0], data[:, col_to_plot])
 
 
-def load_data_A(date, time, angle, col_to_plot):
+def load_data_A(data_path, date, time, angle, col_to_plot):
     file = data_path+str(date)+"_"+h.timestring(time)+"/Fluence/"+str(int(angle))+"/1.0"+"/overviewData.txt"
     data = np.genfromtxt(file, comments="#")
     return(data[:, 0], data[:, col_to_plot])
 
 
-def load_data_B(date, time, angle, col_to_plot):
+def load_data_B(data_path, date, time, angle, col_to_plot):
     file = data_path+str(date)+"_"+h.timestring(time)+"/Fluence/"+str(angle)+"/1.0"+"/overviewData.txt"
     data = np.genfromtxt(file, comments="#")
     return(data[:, 0], data[:, col_to_plot])
 
 
-def load_data_reflectivity(date, time, voltage, col_to_plot):
+def load_data_reflectivity(data_path, date, time, voltage, col_to_plot):
     file = data_path+str(date)+"_"+h.timestring(time)+"/Fluence/-1.0/"+str(voltage)+"/dataBplus.txt"
     data = np.genfromtxt(file, comments="#")
     return(data[:, 0], data[:, col_to_plot])
