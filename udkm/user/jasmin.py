bc0ef72d0457eabc7d74ad3db20f2ccfda089084
Jasmin Jarecki <jarecki@uni-potsdam.de>
2022-04-21 15:25:01 +0200

    small change in moke.functions.export_raw_data

--- udkm/user/jasmin.py
+++ udkm/user/jasmin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import udkm.tools.functions as tools
 
 teststring = "Successfully loaded udkm.user.jasmin"
 
@@ -14,3 +15,27 @@ def smooth_curve(time, trace, steps):
         timeNew[n] = time[(n*steps)+round(1/2*steps)]
     timeNew[-1] = timeNew[-2]
     return(timeNew, traceNew)
+
+
+def load_data(data_path, date, time, voltage, col_to_plot):
+    file = data_path+str(date)+"_"+tools.timestring(time)+"/Fluence/-1.0/"+str(voltage)+"/overviewData.txt"
+    data = np.genfromtxt(file, comments="#")
+    return(data[:, 0], data[:, col_to_plot])
+
+
+def load_data_A(data_path, date, time, angle, col_to_plot):
+    file = data_path+str(date)+"_"+tools.timestring(time)+"/Fluence/"+str(int(angle))+"/1.0"+"/overviewData.txt"
+    data = np.genfromtxt(file, comments="#")
+    return(data[:, 0], data[:, col_to_plot])
+
+
+def load_data_B(data_path, date, time, angle, col_to_plot):
+    file = data_path+str(date)+"_"+tools.timestring(time)+"/Fluence/"+str(angle)+"/1.0"+"/overviewData.txt"
+    data = np.genfromtxt(file, comments="#")
+    return(data[:, 0], data[:, col_to_plot])
+
+
+def load_data_reflectivity(data_path, date, time, voltage, col_to_plot):
+    file = data_path+str(date)+"_"+tools.timestring(time)+"/Fluence/-1.0/"+str(voltage)+"/overviewData.txt"
+    data = np.genfromtxt(file, comments="#")
+    return(data[:, 0], data[:, col_to_plot])
