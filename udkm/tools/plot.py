1677f92efd092f931ea82166adb5fe34f66f9546
Alexander von Reppert <reppert@uni-potsdam.de>
2023-10-11 23:05:11 +0200

    added gif and avi export to plot

--- udkm/tools/plot.py
+++ udkm/tools/plot.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import udkm.tools.colors as colors
+import cv2
+import os
+import imageio
 
 
 def list_plot(x_list, y_list, label_list, **kwargs):
@@ -107,3 +110,67 @@ def offset_plot(x_list, y_list, label_list, offset=0.1, x_text=0, y_text=0, **kw
         ax1.annotate(label_list[index], xy=(x_text, counter*offset+y_text), xycoords='data',
                      xytext=(x_text, counter*offset+y_text), fontsize=8, color=color)
     return ax1
+
+
+def create_video_from_pngs(png_folder, output_file, frame_rate, output_format='avi'):
+    """
+    Create a video (AVI or GIF) from a series of numbered PNG files.
+
+    Parameters
+    ----------
+    png_folder : str
+        The path to the folder containing the numbered PNG files.
+    output_file : str
+        The path to the output video file (AVI or GIF).
+    frame_rate : int
+        The frame rate (frames per second) of the resulting video.
+    output_format : str, optional
+        The output video format ('avi' or 'gif'). Default is 'avi'.
+
+    Returns
+    -------
+    None
+    """
+    # Ensure the folder paths have the correct format (forward slash).
+    png_folder = os.path.normpath(png_folder)
+    output_file = os.path.normpath(output_file)
+
+    # Get the list of PNG files in the folder.
+    png_files = [f for f in os.listdir(png_folder) if f.endswith('.png')]
+    png_files.sort()  # Sort the files to ensure the correct order.
+
+    # Check if there are PNG files to process.
+    if not png_files:
+        print("No PNG files found in the specified folder.")
+        return
+
+    # Load the first PNG image to get the dimensions.
+    first_image = cv2.imread(os.path.join(png_folder, png_files[0]))
+    height, width, layers = first_image.shape
+
+    if output_format == 'avi':
+        # Define the VideoWriter to create the AVI file.
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec for AVI (XVID)
+        out = cv2.VideoWriter(output_file, fourcc, frame_rate, (width, height))
+
+        # Iterate through PNG files and add them to the AVI.
+        for png_file in png_files:
+            image_path = os.path.join(png_folder, png_file)
+            frame = cv2.imread(image_path)
+            out.write(frame)
+
+        # Release the VideoWriter when done.
+        out.release()
+
+    elif output_format == 'gif':
+        # Create a GIF using the imageio library.
+        images = []
+        for png_file in png_files:
+            image_path = os.path.join(png_folder, png_file)
+            frame = imageio.imread(image_path)
+            images.append(frame)
+
+        imageio.mimsave(output_file, images, duration=1/frame_rate)
+
+    else:
+        print("Invalid output format. Use 'avi' or 'gif'.")
